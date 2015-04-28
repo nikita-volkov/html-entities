fe63110cfdc99b5654cf82de7ba5dd3976f443c3
Nikita Volkov <nikita.y.volkov@mail.ru>
2015-04-28 12:58:39 +0300

    Builder

---
+++ library/HTMLEntities/Builder.hs
@@ -0,0 +1,35 @@
+module HTMLEntities.Builder (char, text) where
+
+import HTMLEntities.Prelude
+import qualified Data.Text as Text
+import qualified Data.Text.Lazy.Builder as Text (Builder)
+import qualified Data.Text.Lazy.Builder as Text.Builder
+
+
+-- |
+-- HTML-encodes the given char into a text builder.
+char :: Char -> Text.Builder
+char c =
+  fromMaybe (Text.Builder.singleton c) $
+  lookup c entitiesTable
+
+-- |
+-- HTML-encodes the given text into a text builder.
+-- 
+-- >>> Data.Text.Lazy.IO.putStrLn $ Data.Text.Lazy.Builder.toLazyText $ text "<a href=\"\">"
+-- &lt;a href=&quot;&quot;&gt;
+-- 
+text :: Text -> Text.Builder
+text =
+  Text.foldr (\c b -> char c <> b) mempty
+
+{-# NOINLINE entitiesTable #-}
+entitiesTable :: [(Char, Text.Builder)]
+entitiesTable =
+  [
+    ('<', "&lt;"),
+    ('>', "&gt;"),
+    ('&', "&amp;"),
+    ('"', "&quot;"),
+    ('\'', "&#39;")
+  ]
