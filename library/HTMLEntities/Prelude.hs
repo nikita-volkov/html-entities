275445e8b672bcb0b95bf7a7ed80292a18f327d9
Taylor Fausak <taylor@fausak.me>
2024-01-23 09:23:48 -0600

    Fix build with GHC 9.8

--- library/HTMLEntities/Prelude.hs
+++ library/HTMLEntities/Prelude.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE CPP #-}
+
 module HTMLEntities.Prelude
   ( module Exports,
   )
@@ -25,7 +27,13 @@ import Data.Either as Exports
 import Data.Fixed as Exports
 import Data.Foldable as Exports hiding (toList)
 import Data.Function as Exports hiding (id, (.))
+
+#if MIN_VERSION_base(4, 19, 0)
+import Data.Functor as Exports hiding (unzip)
+#else
 import Data.Functor as Exports
+#endif
+
 import Data.Functor.Compose as Exports
 import Data.IORef as Exports
 import Data.Int as Exports
